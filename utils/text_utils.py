a25f8195114c4adedd5505b5cb06247d839132a3
Monish <53029918+Monishraj50@users.noreply.github.com>
2022-08-15 11:37:05 +0530

    Create text_utils.py

---
+++ utils/text_utils.py
@@ -0,0 +1,25 @@
+
+def remove_punctuation(text_original):
+    text_no_punctuation = text_original.translate(string.punctuation)
+    return(text_no_punctuation)
+
+def remove_single_character(text):
+    text_len_more_than1 = ""
+    for word in text.split():
+        if len(word) > 1:
+            text_len_more_than1 += " " + word
+    return(text_len_more_than1)
+
+def remove_numeric(text):
+    text_no_numeric = ""
+    for word in text.split():
+        isalpha = word.isalpha()
+        if isalpha:
+            text_no_numeric += " " + word
+    return(text_no_numeric)
+
+def text_clean(text_original):
+    text = remove_punctuation(text_original)
+    text = remove_single_character(text)
+    text = remove_numeric(text)
+    return(text)
