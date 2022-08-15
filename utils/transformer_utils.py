f5ae5078cb4ebee2b219c86a90f1dae30bb5e1d1
Monish <53029918+Monishraj50@users.noreply.github.com>
2022-08-15 10:58:33 +0530

    Create transformer_utils.py

---
+++ utils/transformer_utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
+
+def positional_encoding_1d(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                           np.arange(d_model)[np.newaxis, :],
+                           d_model)
+
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+def positional_encoding_2d(row,col,d_model):
+    assert d_model % 2 == 0
+    row_pos = np.repeat(np.arange(row),col)[:,np.newaxis]
+    col_pos = np.repeat(np.expand_dims(np.arange(col),0),row,axis=0).reshape(-1,1)
+
+    angle_rads_row = get_angles(row_pos,np.arange(d_model//2)[np.newaxis,:],d_model//2)
+    angle_rads_col = get_angles(col_pos,np.arange(d_model//2)[np.newaxis,:],d_model//2)
+
+    angle_rads_row[:, 0::2] = np.sin(angle_rads_row[:, 0::2])
+    angle_rads_row[:, 1::2] = np.cos(angle_rads_row[:, 1::2])
+    angle_rads_col[:, 0::2] = np.sin(angle_rads_col[:, 0::2])
+    angle_rads_col[:, 1::2] = np.cos(angle_rads_col[:, 1::2])
+    pos_encoding = np.concatenate([angle_rads_row,angle_rads_col],axis=1)[np.newaxis, ...]
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
