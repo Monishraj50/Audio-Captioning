38d701ad6925c4172344a0b96b255fbc0551fd58
Monish <53029918+Monishraj50@users.noreply.github.com>
2022-08-15 11:01:51 +0530

    Update transformer_utils.py

--- utils/transformer_utils.py
+++ utils/transformer_utils.py
@@ -30,3 +30,23 @@ def positional_encoding_2d(row,col,d_model):
     pos_encoding = np.concatenate([angle_rads_row,angle_rads_col],axis=1)[np.newaxis, ...]
     return tf.cast(pos_encoding, dtype=tf.float32)
 
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask  # (seq_len, seq_len)
+
+def scaled_dot_product_attention(q, k, v, mask):
+    matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9) 
+
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1) 
+    output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
+
+    return output, attention_weights
