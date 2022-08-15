725ba1325aecb7062062512d9b67f61dd7db0018
Monish <53029918+Monishraj50@users.noreply.github.com>
2022-08-15 10:54:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,299 @@
+import string
+import numpy as np
+import pandas as pd
+from numpy import array
+from PIL import Image
+import pickle
+from IPython.display import Audio
+import librosa
+
+import matplotlib.pyplot as plt
+import sys, time, os, warnings
+warnings.filterwarnings("ignore")
+import re
+
+#import keras
+import tensorflow as tf
+from tensorflow import keras
+from tqdm import tqdm
+from nltk.translate.bleu_score import sentence_bleu
+
+from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense, BatchNormalization
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dropout
+from keras.layers.merge import add
+from keras.callbacks import ModelCheckpoint
+from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.text import Tokenizer
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+X_load = np.load("./audiocaps_spec.npz",allow_pickle=True)
+X = X_load['features']
+y = X_load['labels']
+
+print(X[0].shape)
+
+X_n = []
+for i in X:
+    X_n.append(np.expand_dims(i,axis=-1))
+
+vocabulary = []
+for txt in y:
+    vocabulary.extend(txt.split())
+print('Vocabulary Size: %d' % len(set(vocabulary)))
+y_clean=[]
+
+def remove_punctuation(text_original):
+    text_no_punctuation = text_original.translate(string.punctuation)
+    return(text_no_punctuation)
+
+def remove_single_character(text):
+    text_len_more_than1 = ""
+    for word in text.split():
+        if len(word) > 1:
+            text_len_more_than1 += " " + word
+    return(text_len_more_than1)
+
+def remove_numeric(text):
+    text_no_numeric = ""
+    for word in text.split():
+        isalpha = word.isalpha()
+        if isalpha:
+            text_no_numeric += " " + word
+    return(text_no_numeric)
+
+def text_clean(text_original):
+    text = remove_punctuation(text_original)
+    text = remove_single_character(text)
+    text = remove_numeric(text)
+    return(text)
+
+for i, caption in enumerate(y):
+    newcaption = text_clean(caption)
+    y_clean.append(caption)
+
+clean_vocabulary = []
+for txt in y_clean:
+    clean_vocabulary.extend(txt.split())
+print('Clean Vocabulary Size: %d' % len(set(clean_vocabulary)))
+
+all_captions = []
+for caption  in y_clean:
+    caption = '<start> ' + caption+ ' <end>'
+    all_captions.append(caption)
+    
+def load_image(img):
+    #img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.repeat(img, [3], axis=-1, name=None)
+    img = tf.image.resize(img, (299, 299))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return img
+  
+with tf.device('/device:CPU:0'):
+    image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
+    new_input = image_model.input
+    hidden_layer = image_model.layers[-1].output
+    image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+    
+encode_train = X_n
+
+with tf.device('/device:CPU:0'):
+    image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
+    image_dataset = image_dataset.map(
+      load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(2)
+    
+    
+batch_feat = []
+for i,img in enumerate(tqdm(image_dataset)):
+    batch_features = image_features_extract_model(img)
+    batch_features = tf.reshape(batch_features,
+                              (batch_features.shape[0], -1, batch_features.shape[3]))
+    for bf in batch_features:
+        batch_feat.append(bf)
+        
+batch_feat = np.array(batch_feat)
+#np.save('batch_feat.npy', batch_feat)  
+batch_feat = np.load('batch_feat.npy')
+
+top_k = 5000
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
+                                                 oov_token="<unk>",
+                                                 filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+
+tokenizer.fit_on_texts(all_captions)
+train_seqs = tokenizer.texts_to_sequences(all_captions)
+tokenizer.word_index['<pad>'] = 0
+tokenizer.index_word[0] = '<pad>'
+
+train_seqs = tokenizer.texts_to_sequences(all_captions)
+cap_vector = tf.keras.preprocessing.sequence.pad_sequences(train_seqs, padding='post')
+
+batch_feat_train, batch_feat_val, cap_train, cap_val = train_test_split(batch_feat,
+                                                                    cap_vector,
+                                                                    test_size=0.1,
+                                                                    random_state=1432)
+
+BATCH_SIZE = 32
+BUFFER_SIZE = 500
+num_steps = len(batch_feat_train) // BATCH_SIZE
+
+def map_func(img_tensor, cap):
+  #img_tensor = np.load(img_name)
+  return img_tensor, cap
+
+dataset = tf.data.Dataset.from_tensor_slices((batch_feat_train, cap_train))
+
+# Shuffle and batch
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+num_layer = 4
+d_model = 512
+dff = 2048
+num_heads = 8
+row_size = 8
+col_size = 8
+target_vocab_size = top_k + 1
+dropout_rate = 0.1
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, d_model, warmup_steps=4000):
+        super(CustomSchedule, self).__init__()
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+      
+learning_rate = CustomSchedule(d_model)
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                    epsilon=1e-9)
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+    return tf.reduce_sum(loss_)/tf.reduce_sum(mask)
+  
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+transformer = Transformer(num_layer,d_model,num_heads,dff,row_size,col_size,target_vocab_size,max_pos_encoding=target_vocab_size,rate=dropout_rate)
+
+@tf.function
+def train_step(img_tensor, tar):
+    tar_inp = tar[:, :-1]
+    tar_real = tar[:, 1:]
+    dec_mask = create_masks_decoder(tar_inp)
+    with tf.GradientTape() as tape:
+        predictions, _ = transformer(img_tensor, tar_inp,True, dec_mask)
+        loss = loss_function(tar_real, predictions)
+
+    gradients = tape.gradient(loss, transformer.trainable_variables)   
+    optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+    train_loss(loss)
+    train_accuracy(tar_real, predictions)
+    
+    
+ for epoch in range(1):
+    start = time.time()
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    for (batch, (img_tensor, tar)) in enumerate(dataset):
+        train_step(img_tensor, tar)
+        if batch % 50 == 0:
+            print ('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+            epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+
+    print ('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1,
+                                               train_loss.result(),
+                                               train_accuracy.result()))
+    print ('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
+    
+#transformer.save_weights('./audio_caption_transformer.h5')
+#transformer.load_weights("./audio_caption_transformer.h5")
+
+def evaluate(image):
+    #temp_input = tf.expand_dims(load_image(image)[0], 0)
+    #img_tensor_val = image_features_extract_model(temp_input)
+    #img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+  
+    start_token = tokenizer.word_index['<start>']
+    end_token = tokenizer.word_index['<end>']
+   
+    #decoder input is start token.
+    decoder_input = [start_token]
+    output = tf.expand_dims(decoder_input, 0) #tokens
+    result = [] #word list
+
+    for i in range(100):
+        dec_mask = create_masks_decoder(output)
+  
+      # predictions.shape == (batch_size, seq_len, vocab_size)
+        predictions, attention_weights = transformer(image,output,False,dec_mask)
+      
+      # select the last word from the seq_len dimension
+        predictions = predictions[: ,-1:, :]  # (batch_size, 1, vocab_size)
+
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+      # return the result if the predicted_id is equal to the end token
+        if predicted_id == end_token:
+            return result,tf.squeeze(output, axis=0), attention_weights
+      # concatentate the predicted_id to the output which is given to the decoder
+      # as its input.
+        result.append(tokenizer.index_word[int(predicted_id)])
+        output = tf.concat([output, predicted_id], axis=-1)
+
+    return result,tf.squeeze(output, axis=0), attention_weights
+
+  start_token = tokenizer.word_index['<start>']
+end_token = tokenizer.word_index['<end>']
+# select random image from validation data
+rid = np.random.randint(0, len(batch_feat_val))
+image = batch_feat_val[rid]
+real_caption = ' '.join([tokenizer.index_word[i] for i in cap_val[rid] if i not in [0]])
+caption,result,attention_weights = evaluate(image)
+
+first = real_caption.split(' ', 1)[1]
+real_caption = first.rsplit(' ', 1)[0]
+
+#remove "<unk>" in result
+for i in caption:
+    if i=="<unk>":
+        caption.remove(i)
+
+for i in real_caption:
+    if i=="<unk>":
+        real_caption.remove(i)
+
+#remove <end> from result         
+result_join = ' '.join(caption)
+result_final = result_join.rsplit(' ', 1)[0]
+
+real_appn = []
+real_appn.append(real_caption.split())
+reference = real_appn
+candidate = caption
+score = sentence_bleu(reference, candidate, weights=(1.0,0,1.0,0))
+print(f"BLEU-1 score: {score*100}")
+score = sentence_bleu(reference, candidate, weights=(0.5,0.5,0,0))
+print(f"BLEU-2 score: {score*100}")
+score = sentence_bleu(reference, candidate, weights=(0.3,0.3,0.3,0))
+print(f"BLEU-3 score: {score*100}")
+score = sentence_bleu(reference, candidate, weights=(0.25,0.25,0.25,0.25))
+print(f"BLEU-4 score: {score*100}")
+
+print ('Real Caption:', real_caption)
+print ('Predicted Caption:', ' '.join(caption))
